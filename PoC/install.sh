535e7721b0ad5767b86a825e0d9a9ec816972034
Daniele Bortoluzzi <daniele.bortoluzzi@unito.it>
2024-04-15 14:50:13 +0200

    improve install script and add ap_run script

--- PoC/install.sh
+++ PoC/install.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+LOG_PREFIX="[INSTALLER]"
+
+log() {
+    echo "$LOG_PREFIX $@"
+}
+
 compile_package() {
     local component_name="$1"
     
+    log "Build $component_name."
+    
     if [ -d "$component_name" ]; then
-      cd $component_name &&
-        colcon build --event-handlers console_direct+ --symlink-install &&
+      cd $component_name
+	log "Clearing build directories..."
+	rm -rf build/ log/ install/
+	rosdep update
+	rosdep install --from-paths src --ignore-src -r -y -i --os="$OS"
+	log "Build..."
+	colcon build --symlink-install
         cd ..
     else
         echo "Unable to find $component_name"
@@ -29,5 +42,20 @@ components=(
     "Battery"
 )
 
-git submodule update
+# Update submodules
+log "Updating submodules..."
+git submodule update --init --recursive
+log "Submodules updated."
+
+# Initialize rosdep only if it hasn't been initialized before
+if [ ! -f "/etc/ros/rosdep/sources.list.d/20-default.list" ]; then
+    log "Initializing rosdep..."
+    sudo rosdep init
+    log "Rosdep initialized."
+fi
+
+# Declare the OS used
+OS="ubuntu:jammy"
+log "Operating system: $OS"
+
 compile_components "${components[@]}"
